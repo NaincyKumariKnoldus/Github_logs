49c5b46ed2819db02c2df42c35581fabb045c39f
knoldus <naincy.kumari@knoldus.com>
2023-01-03 10:28:51 +0530

    first commit

---
+++ git_commit.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+getCommitResponse=$(curl -s \
+                    -H "Accept: application/vnd.github+json" \
+                    -H "X-GitHub-Api-Version: 2022-11-28" \
+                    https://api.github.com/repos/NaincyKumariKnoldus/Final_Project/commits)
+
+# echo $getCommitResponse
+
+
+# get commit SHA
+commitSHA=$(echo "$getCommitResponse" | \
+            jq '.[].sha'| \
+            tr -d '"')
+
+
+loopCount=$(echo "$commitSHA" | \
+            wc -w)
+
+for (( count=0; count<$loopCount; count++))
+do
+   commitSHA=$(echo "$getCommitResponse" | \
+            jq --argjson count "$count" '.[$count].sha'| \
+            tr -d '"')
+      # get author name
+   authorName=$(echo "$getCommitResponse" | \
+               jq --argjson count "$count" '.[$count].commit.author.name'| \
+               tr -d '"')
+  
+
+   # get commit message
+   commitMessage=$(echo "$getCommitResponse" | \
+               jq --argjson count "$count" '.[$count].commit.message'| \
+               tr -d '"')
+   
+
+   # get commit html url
+   commitHtmlUrl=$(echo "$getCommitResponse" | \
+               jq --argjson count "$count" '.[$count].html_url'| \
+               tr -d '"')
+
+   # get repo name            
+   RepoName=$(echo $commitHtmlUrl | tr -d '"' | cut -d'/' -f5)
+
+   # get commit time
+   commitTime=$(echo "$getCommitResponse" | \
+               jq --argjson count "$count" '.[$count].commit.author.date'| \
+               tr -d '"')
+
+   
+
+
+   # send data to es
+   curl -X POST "http://localhost:9200/github_commit/commit" \
+      -H "Content-Type: application/json" \
+      -d "{ \"commit_sha\" : \"$commitSHA\",
+            \"author_name\" : \"$authorName\",
+            \"commit_message\" : \"$commitMessage\",
+            \"commit_html_url\" : \"$commitHtmlUrl\",
+            \"repo_name\" : \"$RepoName\",
+            \"commit_time\" : \"$commitTime\" }"
+
+
+done
+
---
+++ pr.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+getPrResponse=$(curl -s \
+                    -H "Accept: application/vnd.github+json" \
+                    -H "X-GitHub-Api-Version: 2022-11-28" \
+                    'https://api.github.com/repos/NaincyKumariKnoldus/Final_Project/pulls?state=all&per_page=100&page=1')
+echo $getPrResponse
+# get number of PR
+commitSHA=$(echo "$getPrResponse" | \
+            jq '.[].id'| \
+            tr -d '"')
+
+echo $commitSHA
+
+loopCount=$(echo "$commitSHA" | \
+            wc -w)
+
+echo $loopCount
+
+
+for (( count=0; count<$loopCount; count++))
+do
+   # get PR html url
+  PrHtmlUrl=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].html_url'| \
+               tr -d '"')
+   
+
+   # get PR Body
+  PrBody=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].head.base.repo.name'| \
+               tr -d '"')
+
+
+   # get Repo name
+  RepoName=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].head.repo.name'| \
+               tr -d '"')
+
+# get PR Number
+  PrNumber=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].number'| \
+               tr -d '"')
+  
+
+# get PR Title
+  PrTitle=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].title'| \
+               tr -d '"')
+  
+
+
+# get PR state
+  PrState=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].state'| \
+               tr -d '"')
+  
+
+# get PR created at
+  PrCreatedAt=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].created_at'| \
+               tr -d '"')
+
+
+# get PR closed at
+  PrCloseAt=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].closed_at'| \
+               tr -d '"')
+ 
+
+# get PR merged at
+  PrMergedAt=$(echo "$getPrResponse" | \
+               jq --argjson count "$count" '.[$count].merged_at'| \
+               tr -d '"')
+
+
+# send data to es
+   curl -X POST "http://localhost:9200/github_pull_request/PR" \
+      -H "Content-Type: application/json" \
+      -d "{ \"pr_number\" : \"$PrNumber\",
+            \"pr_url\" : \"$PrHtmlUrl\",
+            \"pr_title\" : \"$PrTitle\",
+            \"repo_name\" : \"$RepoName\",
+            \"pr_body\" : \"$PrBody\",
+            \"pr_state\" : \"$PrState\",
+            \"pr_creation_time\" : \"$PrCreatedAt\",
+            \"pr_closed_time\" : \"$PrCloseAt\",
+            \"pr_merge_at\" : \"$PrMergedAt\"}"
+
+done
+
