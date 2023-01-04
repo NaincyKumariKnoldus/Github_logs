998508171cd13df8aa1ee1351df45f4c2a00069f
aamir7knoldus <aamir.shahab@knoldus.com>
2023-01-04 14:00:04 +0530

    Refactored

--- git_commit.sh
+++ git_commit.sh
@@ -1,57 +1,49 @@
 #!/bin/bash
 
 getCommitResponse=$(curl -s \
-                    -H "Accept: application/vnd.github+json" \
-                    -H "X-GitHub-Api-Version: 2022-11-28" \
-                    https://api.github.com/repos/NaincyKumariKnoldus/Github_logs/commits?sha=$GITHUB_REF_NAME)
+   -H "Accept: application/vnd.github+json" \
+   -H "X-GitHub-Api-Version: 2022-11-28" \
+   https://api.github.com/repos/NaincyKumariKnoldus/Github_logs/commits?sha=$GITHUB_REF_NAME)
 
 # echo $getCommitResponse
 
-
 # get commit SHA
-commitSHA=$(echo "$getCommitResponse" | \
-            jq '.[].sha'| \
-            tr -d '"')
+commitSHA=$(echo "$getCommitResponse" |
+   jq '.[].sha' |
+   tr -d '"')
 
+loopCount=$(echo "$commitSHA" |
+   wc -w)
 
-loopCount=$(echo "$commitSHA" | \
-            wc -w)
-
-for (( count=0; count<$loopCount; count++))
-do
-   commitSHA=$(echo "$getCommitResponse" | \
-            jq --argjson count "$count" '.[$count].sha'| \
-            tr -d '"')
-      # get author name
-   authorName=$(echo "$getCommitResponse" | \
-               jq --argjson count "$count" '.[$count].commit.author.name'| \
-               tr -d '"')
-  
+for ((count = 0; count < $loopCount; count++)); do
+   commitSHA=$(echo "$getCommitResponse" |
+      jq --argjson count "$count" '.[$count].sha' |
+      tr -d '"')
+   # get author name
+   authorName=$(echo "$getCommitResponse" |
+      jq --argjson count "$count" '.[$count].commit.author.name' |
+      tr -d '"')
 
    # get commit message
-   commitMessage=$(echo "$getCommitResponse" | \
-               jq --argjson count "$count" '.[$count].commit.message'| \
-               tr -d '"')
-   
+   commitMessage=$(echo "$getCommitResponse" |
+      jq --argjson count "$count" '.[$count].commit.message' |
+      tr -d '"')
 
    # get commit html url
-   commitHtmlUrl=$(echo "$getCommitResponse" | \
-               jq --argjson count "$count" '.[$count].html_url'| \
-               tr -d '"')
+   commitHtmlUrl=$(echo "$getCommitResponse" |
+      jq --argjson count "$count" '.[$count].html_url' |
+      tr -d '"')
 
-   # get repo name            
+   # get repo name
    RepoName=$(echo $commitHtmlUrl | tr -d '"' | cut -d'/' -f5)
 
    # get commit time
-   commitTime=$(echo "$getCommitResponse" | \
-               jq --argjson count "$count" '.[$count].commit.author.date'| \
-               tr -d '"')
-
-   
-
+   commitTime=$(echo "$getCommitResponse" |
+      jq --argjson count "$count" '.[$count].commit.author.date' |
+      tr -d '"')
 
    # send data to es
-   curl -X POST "https://5172-103-97-214-105.in.ngrok.io/github_commit/commit" \
+   curl -X POST "$ES_URL/github_commit/commit" \
       -H "Content-Type: application/json" \
       -d "{ \"commit_sha\" : \"$commitSHA\",
             \"author_name\" : \"$authorName\",
@@ -61,6 +53,4 @@ do
             \"repo_name\" : \"$RepoName\",
             \"commit_time\" : \"$commitTime\" }"
 
-
 done
-
--- pr.sh
+++ pr.sh
@@ -1,83 +1,72 @@
 #!/bin/bash
 
 getPrResponse=$(curl -s \
-                    -H "Accept: application/vnd.github+json" \
-                    -H "X-GitHub-Api-Version: 2022-11-28" \
-                    'https://api.github.com/repos/NaincyKumariKnoldus/Github_logs/pulls?state=all&per_page=100&page=1')
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  'https://api.github.com/repos/NaincyKumariKnoldus/Github_logs/pulls?state=all&per_page=100&page=1')
 echo $getPrResponse
 # get number of PR
-commitSHA=$(echo "$getPrResponse" | \
-            jq '.[].id'| \
-            tr -d '"')
+commitSHA=$(echo "$getPrResponse" |
+  jq '.[].id' |
+  tr -d '"')
 
 echo $commitSHA
 
-loopCount=$(echo "$commitSHA" | \
-            wc -w)
+loopCount=$(echo "$commitSHA" |
+  wc -w)
 
 echo $loopCount
 
+for ((count = 0; count < $loopCount; count++)); do
+  # get PR html url
+  PrHtmlUrl=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].html_url' |
+    tr -d '"')
 
-for (( count=0; count<$loopCount; count++))
-do
-   # get PR html url
-  PrHtmlUrl=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].html_url'| \
-               tr -d '"')
-   
+  # get PR Body
+  PrBody=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].head.base.repo.name' |
+    tr -d '"')
 
-   # get PR Body
-  PrBody=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].head.base.repo.name'| \
-               tr -d '"')
+  # get Repo name
+  RepoName=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].head.repo.name' |
+    tr -d '"')
 
+  # get PR Number
+  PrNumber=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].number' |
+    tr -d '"')
 
-   # get Repo name
-  RepoName=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].head.repo.name'| \
-               tr -d '"')
+  # get PR Title
+  PrTitle=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].title' |
+    tr -d '"')
 
-# get PR Number
-  PrNumber=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].number'| \
-               tr -d '"')
-  
+  # get PR state
+  PrState=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].state' |
+    tr -d '"')
 
-# get PR Title
-  PrTitle=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].title'| \
-               tr -d '"')
-  
+  # get PR created at
+  PrCreatedAt=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].created_at' |
+    tr -d '"')
 
+  # get PR closed at
+  PrCloseAt=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].closed_at' |
+    tr -d '"')
 
-# get PR state
-  PrState=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].state'| \
-               tr -d '"')
-  
+  # get PR merged at
+  PrMergedAt=$(echo "$getPrResponse" |
+    jq --argjson count "$count" '.[$count].merged_at' |
+    tr -d '"')
 
-# get PR created at
-  PrCreatedAt=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].created_at'| \
-               tr -d '"')
-
-
-# get PR closed at
-  PrCloseAt=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].closed_at'| \
-               tr -d '"')
- 
-
-# get PR merged at
-  PrMergedAt=$(echo "$getPrResponse" | \
-               jq --argjson count "$count" '.[$count].merged_at'| \
-               tr -d '"')
-
-
-# send data to es
-   curl -X POST "https://5172-103-97-214-105.in.ngrok.io/github_pull_request/PR" \
-      -H "Content-Type: application/json" \
-      -d "{ \"pr_number\" : \"$PrNumber\",
+  # send data to es
+  curl -X POST "$ES_URL/github_pull_request/PR" \
+    -H "Content-Type: application/json" \
+    -d "{ \"pr_number\" : \"$PrNumber\",
             \"pr_url\" : \"$PrHtmlUrl\",
             \"pr_title\" : \"$PrTitle\",
             \"repo_name\" : \"$RepoName\",
@@ -88,4 +77,3 @@ do
             \"pr_merge_at\" : \"$PrMergedAt\"}"
 
 done
-
