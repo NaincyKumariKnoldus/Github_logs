9d38663e36ba0a6d72e31823be7b1e65422744e2
NaincyKumariKnoldus <87004609+NaincyKumariKnoldus@users.noreply.github.com>
2023-01-06 16:10:55 +0530

    Update git_pr.sh

--- git_pr.sh
+++ git_pr.sh
@@ -4,7 +4,7 @@
 getPrResponse=$(curl -s \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "https://api.github.com/repos//NaincyKumariKnoldus/Github_logs/pulls?state=all&per_page=100&page=1")
+  "https://api.github.com/repos/NaincyKumariKnoldus/Github_logs/pulls?state=all&per_page=100&page=1")
 
 # get number of PR
 totalPR=$(echo "$getPrResponse" |
