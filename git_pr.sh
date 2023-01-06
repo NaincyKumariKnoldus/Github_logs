1acb3e5acc19e7394bfb5dbc83d6346ca7ee6269
NaincyKumariKnoldus <87004609+NaincyKumariKnoldus@users.noreply.github.com>
2023-01-06 16:10:19 +0530

    Update git_pr.sh

--- git_pr.sh
+++ git_pr.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+# get github PR details
 getPrResponse=$(curl -s \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  'https://api.github.com/repos/NaincyKumariKnoldus/Github_logs/pulls?state=all&per_page=100&page=1')
+  "https://api.github.com/repos/NaincyKumariKnoldus/Github_logs/pulls?state=all&per_page=100&page=1")
 
 # get number of PR
 totalPR=$(echo "$getPrResponse" |
   jq '.[].number' |
   tr -d '"')
 
+# get the loop count based on number of PRs
 loopCount=$(echo "$totalPR" |
   wc -w)
 echo "loopcount= $loopCount"
 
-rm -rf sha_es.txt
 # get data from ES
 getEsPR=$(curl -H "Content-Type: application/json" -X GET "$ES_URL/github_pr/_search?pretty" -d '{
                   "size": 10000,                                                                  
@@ -26,21 +27,22 @@ getEsPR=$(curl -H "Content-Type: application/json" -X GET "$ES_URL/github_pr/_se
                   jq '.hits.hits[]._source.pr_number' |
                   tr -d '"')
 
-echo $getEsPR | tr " " "\n" >sha_es.txt
-rm -rf match.txt
-rm -rf unmatch.txt
+# store ES PR number in a temp file
+echo $getEsPR | tr " " "\n" > sha_es.txt
 
+# looping through each PR detail
 for ((count = 0; count < $loopCount; count++)); do
 
   # get PR_number
   totalPR=$(echo "$getPrResponse" |
     jq --argjson count "$count" '.[$count].number' |
     tr -d '"')
-
+  
+  # looping through each PR detail
   matchRes=$(grep -o $totalPR sha_es.txt)
   echo $matchRes | tr " " "\n" >>match.txt
-  echo $matchRes
 
+  # filtering and pushing unmatched PR number details to ES
   if [ -z $matchRes ]; then
     # get PR html url
     PrHtmlUrl=$(echo "$getPrResponse" |
@@ -99,31 +101,16 @@ for ((count = 0; count < $loopCount; count++)); do
             \"pr_url\" : \"$PrHtmlUrl\",
             \"pr_title\" : \"$PrTitle\",
             \"pr_body\" : \"$PrBody\",
-            \"repo_name\" : \"$RepoName\",
             \"pr_base_branch\" : \"$PrBaseBranch\",
             \"pr_source_branch\" : \"$PrSourceBranch\",
             \"pr_state\" : \"$PrState\",
             \"pr_creation_time\" : \"$PrCreatedAt\",
             \"pr_closed_time\" : \"$PrCloseAt\",
             \"pr_merge_at\" : \"$PrMergedAt\"}"
-  else
-    echo non-empty_skip
   fi
 done
-# remove temporary file
-echo "Data From ES"
-echo "ES_Data_count:"
-cat sha_es.txt | wc -l
-cat sha_es.txt
-echo
-echo "-------------------------------------------"
-echo "Match data from ES"
-echo "Match data count:"
-cat match.txt | wc -l
-cat match.txt
-echo
-echo "-------------------------------------------"
-echo "UnMatch data from ES"
-echo "UnMatch data count:"
-cat unmatch.txt | wc -l
-cat unmatch.txt
+
+# removing temporary file
+rm -rf sha_es.txt
+rm -rf match.txt
+rm -rf unmatch.txt
